1c583d16500e5cdc4005031bcbcee54b4e43a7aa
annTerry <annak@1gb.ru>
2022-04-28 00:53:36 +0300

    feat: binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,40 +7,107 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  treeObject = null;
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      return this.treeObject;
+  }
+  createNode(data) {
+      return { data: data, left: null, right: null }
+  }
+  searchInObject(data, tree) {
+      if (tree === null) {
+          return this.createNode(data);
+      }
+      if (tree.data > data) {
+          tree.right = this.searchInObject(data, tree.right);
+
+      }
+      else if (tree.data < data) {
+          tree.left = this.searchInObject(data, tree.left);
+      }
+      return tree;
+  }
+  add(data, tree = this.treeObject) {
+      this.treeObject = this.searchInObject(data, tree);
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data, tree = this.treeObject) {
+      if (tree == null) return false;
+      if (tree.data > data) {
+          return this.has(data, tree.right);
+      }
+      if (tree.data < data) {
+          return this.has(data, tree.left);
+      } 
+      return true;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data, tree = this.treeObject) {
+      if (tree == null) return null;
+      if (tree.data > data) {
+          return this.find(data, tree.right);
+      }
+      else if (tree.data < data) {
+          return this.find(data, tree.left);
+      }
+      return tree;
+  }
+  hardRemove(treeToFind, treeToConnect) {
+      if (treeToFind.right == null) {
+          treeToFind.right = treeToConnect;
+          return { "connect": treeToFind, "prev": treeToFind.left };
+      }
+      let newNode = this.hardRemove(treeToFind.right, treeToConnect);
+      if (newNode.prev) {
+          treeToFind.right = newNode.prev;
+      }
+      return { "connect": newNode.connect, "prev": treeToFind };
+  }
+  remove(data, tree = this.treeObject) {
+      if (tree == null) return null;
+      if (tree.data > data) {
+          tree.right = this.remove(data, tree.right);
+      }
+      else if (tree.data < data) {
+          tree.left = this.remove(data, tree.left);
+      }
+      else {
+          if (!tree.left && !tree.right) {
+              return null;
+          }
+          else if (tree.left && !tree.right) {
+              return tree.left;
+          }
+          else if (!tree.left && tree.right) {
+              return tree.right;
+          }
+          else {
+              let newNode = this.hardRemove(tree.left, tree.right);
+              newNode.connect.left = newNode.prev;
+              if(this.treeObject.data==data)
+              {
+                this.treeObject=newNode.connect;
+              }
+              return newNode.connect;
+          }
+      }
+      return tree;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  min(tree = this.treeObject) {
+      if (tree == null) return null;
+      if (tree.right) {
+          return this.min(tree.right)
+      }
+      return tree.data;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  max(tree = this.treeObject) {
+      if (tree == null) return null;
+      if (tree.left) {
+          return this.max(tree.left)
+      }
+      return tree.data;
   }
 }
 
